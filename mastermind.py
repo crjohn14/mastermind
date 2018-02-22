6addc802fbce4701b862951fa9f89646502a7c61
Chris Johnson <cjohnson@cm.utexas.edu>
2018-02-22 15:52:00 -0600

    added functions to mastermind and added .gitignore

--- mastermind.py
+++ mastermind.py
@@ -1,55 +1,85 @@
 import requests, json, sys
-if sys.version_info < (3,0):
-  sys.exit('Python version < 3.0 does not support modern TLS versions. You will have trouble connecting to our API using Python 2.X.')
-email = 'cjohnson@cm.utexas.edu' # my email
-r = requests.post('https://mastermind.praetorian.com/api-auth-token/', data={'email':email})
-r.json()
-headers = r.json()  # > {'Auth-Token': 'AUTH_TOKEN'}
-headers['Content-Type'] = 'application/json'
-
-# play the game
-done = False
-level = 1
-while(not done):
-  # request level info from the mastermind
-  r = requests.get('https://mastermind.praetorian.com/level/{0}/'.format(level), headers=headers)
-  lvlinfo = r.json()  # > {'numGladiators': 4, 'numGuesses': 8, 'numRounds': 1, 'numWeapons': 6}
-
-  # print level info
-  print('# of rounds: {0[numRounds]:d}'.format(lvlinfo))
-  print('# of guesses: {0[numGuesses]:d}'.format(lvlinfo))
-  print('# of weapons: {0[numWeapons]:d}'.format(lvlinfo))
-  print('# of gladiators: {0[numGladiators]:d}'.format(lvlinfo))
-
-  # defeat the gladiators!!!
-  for guessNum in range(0, lvlinfo['numGuesses']):
-    # input integers separated by commas
-    weapon = input('Choose your weapons!:')
-    guess = list(map(int, weapon.split(',')))
-    # accept judgement from the mastermind
-    r = requests.post('https://mastermind.praetorian.com/level/1/', data=json.dumps({'guess': [1, 2, 3, 4]}), headers=headers)
-    judgement = r.json()  # > {'response': [2, 1]}
-
-    # level up and break if all gladiators are defeated
-    if (judgement['response'][0] == lvlinfo['numWeapons']) and (judgement['response'][1] == lvlinfo['numGladiators']):
-      print('YOU HAVE SLAIN YOUR OPPONENTS!!!')
-      print('The mastermind allows you to continue to the next level')
-      level =+ 1
-      break
-    else:
-      # print judgement
-      print('# of correct weapons: {0} / {1}'.format(judgement['response'][0], lvlinfo['numWeapons']))
-      print('# of correct gladiators: {0} / {1}'.format(judgement['response'][1], lvlinfo['numGladiators']))
 
 
-  else:
-    # did not complete level
-    pass
+def main():
+    # check Python version
+    if sys.version_info < (3, 0):
+        sys.exit('Python version < 3.0 does not support modern TLS versions. You will have trouble connecting to our API using Python 2.X.')
 
-# VICTORY - now get the hash
-r = requests.post('https://mastermind.praetorian.com/hash/', headers=headers)
+    # initialize headers
+    headers = get_header()
+
+    # play the game
+    done = False
+    level = 1
+    while(not done):
+        # request level info from the mastermind
+        r = requests.get('https://mastermind.praetorian.com/level/{0}/'.format(level), headers=headers)
+        lvl_info = r.json()  # > {'numGladiators': 4, 'numGuesses': 8, 'numRounds': 1, 'numWeapons': 6}
+        print_level_info(level, lvl_info)
+
+        # defeat the gladiators!!!
+        while(True):
+            # input integers separated by commas; weapons begin from 0
+            weapon = input('Choose your weapons!:')
+            guess = list(map(int, weapon.split(',')))
+            # accept judgement from the mastermind
+            r = requests.post('https://mastermind.praetorian.com/level/1/', data=json.dumps({'guess': guess}), headers=headers)
+            judgement = r.json()  # > {'response': [2, 1]}
+
+            print(judgement) # DEBUG
+
+            if 'response' in judgement:
+                # print judgement
+                print('correct weapons: {0} / {1}'.format(judgement['response'][0], lvl_info['numWeapons']))
+                print('correct gladiators: {0} / {1}'.format(judgement['response'][1], lvl_info['numGladiators']))
+            elif 'error' in judgement:
+                # error; probably because 10 sec passed
+                print(judgement['error'])
+                break
+            elif 'message' in judgement:
+                print('YOU HAVE SLAIN YOUR OPPONENTS!!!')
+                print('The mastermind allows you to continue to the next level')
+                level += 1
+                break
+            else:
+                print('Judgement was not prepared for.  Code more...')
+                sys.exit(0)
+
+    # VICTORY - now get the hash
+    r = requests.post('https://mastermind.praetorian.com/hash/', headers=headers)
+    hash = r.json()
+    print('Victory is yours.')
+    print('Hash: {}'.format(hash['hash']))
+    print(hash)
+
+
+def get_header():
+    email = 'cjohnson@cm.utexas.edu'  # my email
+    r = requests.post('https://mastermind.praetorian.com/api-auth-token/', data={'email': email})
+    r.json()
+    headers = r.json()  # > {'Auth-Token': 'AUTH_TOKEN'}
+    headers['Content-Type'] = 'application/json'
+    return headers
+
+
+
+def print_level_info(level, lvl_info):
+    print()
+    print('LEVEL {0}'.format(level))
+    print('rounds: {0[numRounds]:d}'.format(lvl_info))
+    print('guesses: {0[numGuesses]:d}'.format(lvl_info))
+    print('weapons: {0[numWeapons]:d}'.format(lvl_info))
+    print('gladiators: {0[numGladiators]:d}'.format(lvl_info))
+    print()
+
+if __name__ == "__main__":
+    main()
 
 # TODO
+# KEYERROR: 'response' line 34
+#   - happened after 4th guess
+#   - happened when guessing after waiting 10 sec (cursor also stopped blinking)
 # error message if run out of turns
 # message to continue if ran out of turns
 # deal with multiple rounds per level?
